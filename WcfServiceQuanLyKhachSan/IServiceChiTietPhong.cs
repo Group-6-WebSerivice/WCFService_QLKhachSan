a52286e7485934ff370e44493a2ec1016b1d1caa
Delelope <tanpolo1706@gmail.com>
2015-11-14 15:45:42 +0700

    Ninth commit
    Commit WCFService lớp ChiTietPhong

---
+++ WcfServiceQuanLyKhachSan/IServiceChiTietPhong.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceQuanLyKhachSan
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceChiTietPhong" in both code and config file together.
+    [DataContract]
+    public class ChiTietPhongDTO
+    {
+        [DataMember]
+        public string Maphong;
+        [DataMember]
+        public string Maloai;
+        [DataMember]
+        public Boolean Dadat;
+        [DataMember]
+        public Boolean Danhan;
+        [DataMember]
+        public decimal Gia;
+        [DataMember]
+        public int Songuoi;
+        [DataMember]
+        public string Anhbia;
+        
+    }
+    [ServiceContract]
+    public interface IServiceChiTietPhong
+    {
+        
+        [OperationContract]
+        IList<ChiTietPhongDTO> getListChiTietPhongAll();
+        [OperationContract]
+        ChiTietPhongDTO getListChiTietPhongById(string id);
+        
+    }
+}
---
+++ WcfServiceQuanLyKhachSan/ServiceChiTietPhong.svc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Collections;
+
+namespace WcfServiceQuanLyKhachSan
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceChiTietPhong" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceChiTietPhong.svc or ServiceChiTietPhong.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceChiTietPhong : IServiceChiTietPhong
+    {
+        HotelDataContext htDataContext = new HotelDataContext();
+        public IList<ChiTietPhongDTO> getListChiTietPhongAll()
+        {
+            var query = (from p in htDataContext.phongs join lp in htDataContext.loaiphongs on p.maloai equals lp.maloai
+                        
+                         orderby p.maphong descending
+                         select new ChiTietPhongDTO
+                         {
+                             Maphong = p.maphong,
+                             Maloai = p.maloai,
+                             Dadat = (bool)p.dadat,
+                             Danhan = (bool)p.danhan,
+                             Gia = (decimal)lp.gia,
+                             Songuoi = (int)lp.songuoi,
+                             Anhbia = lp.anhbia,
+                         }).Distinct<ChiTietPhongDTO>();
+
+            return query.ToList<ChiTietPhongDTO>();
+        }
+
+
+        public ChiTietPhongDTO getListChiTietPhongById(string id)
+        {
+            var querry = (from p in htDataContext.phongs join lp in htDataContext.loaiphongs on p.maloai equals lp.maloai
+                          where p.maphong == id
+                          select new ChiTietPhongDTO
+                          {
+                              Maphong = p.maphong,
+                              Maloai = p.maloai,
+                              Dadat = (bool)p.dadat,
+                              Danhan = (bool)p.danhan,
+                              Gia = (decimal)lp.gia,
+                              Songuoi = (int)lp.songuoi,
+                              Anhbia = lp.anhbia,
+                          }).Distinct<ChiTietPhongDTO>();
+            if (querry.Count() == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return querry.Single<ChiTietPhongDTO>();
+            }
+        }
+    }
+}
